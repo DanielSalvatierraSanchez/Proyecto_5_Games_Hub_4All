1349bfff57305778bfe34b0b2433f4fbf4c87bfa
Daniel Salvatierra Sanchez <147101731+DanielSalvatierraSanchez@users.noreply.github.com>
2024-04-19 01:48:54 +0200

    Add files via upload
    Eliminar de main.js líneas innecesarias de código.

--- main.js
+++ main.js
@@ -2,21 +2,15 @@ import './style.css';
 import { Header } from "./src/components/Header/header";
 import { createMenuGames } from "./src/components/Menu/menuGames";
 import { Footer } from "./src/components/Footer/footer";
-import { createMemory } from './src/pages/MemoryGame/memoryGame';
-import { buttonOfReset } from './src/pages/MemoryGame/Reset/buttonReset';
-import { createPpt } from './src/pages/PPT/ppt';
-import { createBingo } from './src/pages/Bingo/bingo';
 
+// seleccionar contenedor
 const divApp = document.querySelector("#app")
 
+// crear contenedor de los games
 const divContainer = document.createElement('div');
 divContainer.id = 'divContainer';
 document.body.appendChild(divContainer);
 
 Header();
 createMenuGames(divApp);
-//createMemory(divApp, divContainer);
-//buttonOfReset(divContainer, divApp);
-//createPpt(divApp, divContainer);
-//createBingo(divApp, divContainer);
 Footer();
