d67707445cb471c0ffb7b9c25ca6fc76517ad627
Daniel Salvatierra Sanchez <147101731+DanielSalvatierraSanchez@users.noreply.github.com>
2024-05-12 10:41:46 +0200

    Delete src directory

--- src/pages/MemoryGame/LocalStorage/localStorage.js
+++
@@ -1,8 +0,0 @@
-export const saveGameState = (pairsCount) => {
-  localStorage.setItem('savePairsCount', pairsCount);
-};
-
-export const loadGameState = () => {
-  const savedPairsCount = localStorage.getItem('savePairsCount');
-  return savedPairsCount ? parseInt(savedPairsCount) : 0;
-};
--- src/pages/MemoryGame/WinGame/winGame.js
+++
@@ -1,9 +0,0 @@
-export const winGame = (divCard) => {
-  divCard = document.createElement('div');
-  divCard.innerHTML = "";
-  const divContainer = document.querySelector('#divContainer');
-  divContainer.innerHTML = `
-  <h2 class="win-memory-game" >¡¡¡ HAS GANADO !!! JUEGO TERMINADO</h2>
-  <h2 class="win-memory-game" >CARGANDO NUEVA PARTIDA...</h2>
-  `;
-};
