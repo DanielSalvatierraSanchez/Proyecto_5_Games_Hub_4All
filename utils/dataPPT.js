4347c5078bd318fdc429ee1717a8ebdbf9e6673e
Daniel Salvatierra Sanchez <147101731+DanielSalvatierraSanchez@users.noreply.github.com>
2024-05-24 02:27:54 +0200

    Add files via upload

--- utils/dataPPT.js
+++ utils/dataPPT.js
@@ -1,5 +1,6 @@
-export const buttons = () => [
-  { stone: 0, image: '../ppt/piedra.webp', classButton: 'button-stone', classImage: 'image-stone', },
-  { paper: 1, image: '../ppt/papel.webp', classButton: 'button-paper', classImage: 'image-paper', },
-  { scissors: 2, image: '../ppt/tijera.webp', classButton: 'button-scissors', classImage: 'image-scissors', }
+export const buttons = [
+  { value: "rock", image: '../ppt/rock.webp', classButton: 'button-rock', classImage: 'image-rock', },
+  { value: "paper", image: '../ppt/paper.webp', classButton: 'button-paper', classImage: 'image-paper', },
+  { value: "scissors", image: '../ppt/scissors.webp', classButton: 'button-scissors', classImage: 'image-scissors', }
 ];
+//value se cambia por rock paper scissors
